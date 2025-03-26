42c24888c42269df2e87fbac88063aa013d52590
Rayakame <github.android206@passmail.net>
2025-03-26 20:34:19 +0100

    feat(python): Add IndentWriter class and refactor Escape and IsReserved functions (main)

--- scripts/py/reserved_keywoards.py
+++ scripts/py/reserved_keywoards.py
@@ -5,33 +5,58 @@ from pathlib import Path
 kw_list = keyword.kwlist
 path = Path(__file__).parent.parent.parent / "internal" / "core" / "reserved.go"
 
+class IndentWriter:
+
+    def __init__(self, file_path: Path, *, indent_char: str = " ", indent_amount: int = 4):
+        self.file_path = file_path
+        self.lines: list[tuple[str, int]] = []
+        self.indent_char = indent_char
+        self.indent_amount = indent_amount
+
+
+    def write_line(self, text: str, indent_depth: int = 0):
+        self.lines.append((text + "\n", indent_depth))
+
+    def write_blank(self):
+        self.lines.append(("", 0))
+
+    def write_file(self):
+        with open(self.file_path, "w") as file:
+            for line in self.lines:
+                indent: str = (self.indent_char * self.indent_amount) * line[1]
+                file.write(indent + line[0])
+
+
 if __name__ == "__main__":
+    writer = IndentWriter(path)
 
-    with open(path, "w") as go_file:
-        # Write the function header for escape
-        go_file.write(f"""// Package core Auto-generated using python; DO NOT EDIT
-// py {sys.version}
-package core""")
-        go_file.write("""
-func Escape(s string) string {
-    if IsReserved(s) {
-        return s + "_"
-    }
-    return s
-}
+    writer.write_line(f"// Package core Auto-generated using python; DO NOT EDIT")
+    writer.write_line(f"// py {sys.version}")
+    writer.write_line("package core")
+    writer.write_blank()
 
-func IsReserved(s string) bool {
-    switch s {
-""")
+    # Write Escape function
+    writer.write_line("func Escape(s string) string {")
+    writer.write_line("if IsReserved(s) {", 1)
+    writer.write_line("return s + \"_\"", 2)
+    writer.write_line("}", 1)
+    writer.write_line("return s", 1)
+    writer.write_line("}")
+    writer.write_blank()
 
-        for kw in kw_list:
-            go_file.write(f'    case "{kw}":\n')
-            go_file.write("        return true\n")
+    # Write IsReserved function
+    writer.write_line("func IsReserved(s string) bool {")
+    writer.write_line("switch s {", 1)
 
-        go_file.write("""    default:
-            return false
-    }
-}
-""")
+    for kw in kw_list:
+        writer.write_line(f'case "{kw}":', 2)
+        writer.write_line("return true", 3)
 
-    print("Go file 'reserved.go' has been generated.")
+    writer.write_line("default:", 2)
+    writer.write_line("return false", 3)
+    writer.write_line("}", 1)
+    writer.write_line("}")
+
+    # Write to file
+    writer.write_file()
+    print(f"Go file '{path.name}' has been generated.")
