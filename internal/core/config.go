21b2dd379287952f9bb90c4763ab921f1afd1d1b
Rayakame <github.android206@passmail.net>
2025-03-26 18:58:34 +0000

    feat(config): add initialisms map and set default initialisms
    - Add InitialismsMap to store initialisms as map
    - Set default initialisms to ["id"]
    - Update ParseConfig function to handle initialisms
    
    (main)

--- internal/core/config.go
+++ internal/core/config.go
@@ -7,10 +7,16 @@ import (
 )
 
 type Config struct {
-	SqlDriver string `json:"sql_driver" yaml:"sql_driver"`
-	ModelType string `json:"model_type" yaml:"model_type"`
+	SqlDriver                   string    `json:"sql_driver" yaml:"sql_driver"`
+	ModelType                   string    `json:"model_type" yaml:"model_type"`
+	Initialisms                 *[]string `json:"initialisms,omitempty" yaml:"initialisms"`
+	EmitExactTableNames         bool      `json:"emit_exact_table_names,omitempty" yaml:"emit_exact_table_names"`
+	InflectionExcludeTableNames []string  `json:"inflection_exclude_table_names,omitempty" yaml:"inflection_exclude_table_names"`
+	OmitUnusedStructs           bool      `json:"omit_unused_structs,omitempty" yaml:"omit_unused_structs"`
+	QueryParameterLimit         *int32    `json:"query_parameter_limit,omitempty" yaml:"query_parameter_limit"`
 
-	Async bool
+	InitialismsMap map[string]struct{} `json:"-" yaml:"-"`
+	Async          bool
 }
 
 func ParseConfig(req *plugin.GenerateRequest) (*Config, error) {
@@ -34,6 +40,15 @@ func ParseConfig(req *plugin.GenerateRequest) (*Config, error) {
 	}
 	if err := isModelTypeValid(config.ModelType); err != nil {
 		return nil, fmt.Errorf("invalid options: %s", err)
+	}
+	if config.Initialisms == nil {
+		config.Initialisms = new([]string)
+		*config.Initialisms = []string{"id"}
+	}
+
+	config.InitialismsMap = map[string]struct{}{}
+	for _, initial := range *config.Initialisms {
+		config.InitialismsMap[initial] = struct{}{}
 	}
 	return &config, nil
 }
