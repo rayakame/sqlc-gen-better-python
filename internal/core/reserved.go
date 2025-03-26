71bb8f70de0ef2e1c2131a351e733d331d671e79
Rayakame <github.android206@passmail.net>
2025-03-26 20:55:59 +0100

    feat(core): add Escape function for reserved words
    - Add Escape function to handle reserved words by appending an underscore
      to the input string if it is reserved.
    
    ISSUE-1234 (main)

--- internal/core/reserved.go
+++ internal/core/reserved.go
@@ -1,3 +1,5 @@
+// Package core Auto-generated using python; DO NOT EDIT
+// py 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)]
 package core
 
 func Escape(s string) string {
