d8c97a0dcc27e8cd729597da70680d437ef4e1ef
Rayakame <github.android206@passmail.net>
2025-03-26 18:55:22 +0000

    feat: Add script to generate Go file with reserved keywords
    A script was added to generate a Go file containing reserved keywords
    from Python. The generated file includes functions to escape and check
    reserved keywords. (main)

---
+++ scripts/py/reserved_keywoards.py
@@ -0,0 +1,37 @@
+import keyword
+import sys
+from pathlib import Path
+
+kw_list = keyword.kwlist
+path = Path(__file__).parent.parent.parent / "internal" / "core" / "reserved.go"
+
+if __name__ == "__main__":
+
+    with open(path, "w") as go_file:
+        # Write the function header for escape
+        go_file.write(f"""package core
+// Auto-generated using python; DO NOT EDIT
+// py {sys.version}""")
+        go_file.write("""
+func Escape(s string) string {
+    if IsReserved(s) {
+        return s + "_"
+    }
+    return s
+}
+
+func IsReserved(s string) bool {
+    switch s {
+""")
+
+        for kw in kw_list:
+            go_file.write(f'    case "{kw}":\n')
+            go_file.write("        return true\n")
+
+        go_file.write("""    default:
+            return false
+    }
+}
+    """)
+
+    print("Go file 'reserved.go' has been generated.")
