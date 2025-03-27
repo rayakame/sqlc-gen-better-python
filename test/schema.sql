a84d159ae2c76c01a09e7fe682317c4d839568a4
Rayakame <github.android206@passmail.net>
2025-03-27 01:34:23 +0100

    docs: update schema.sql to remove NOT NULL constraint on name field (#main)

--- test/schema.sql
+++ test/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE authors (
                          id   INTEGER PRIMARY KEY,
-                         name text    NOT NULL,
+                         name text  ,
                          bio  text
 );
