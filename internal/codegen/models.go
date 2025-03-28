ae22831a6b8a2a1740343a8adac3c02a8ec1c146
Rayakame <github.android206@passmail.net>
2025-03-28 12:40:30 +0000

    feat(models.go): add support for building model headers and columns
    Added support for building model headers and columns based on the provided
    table data. Handles different model types such as dataclass and attrs. Returns
    the generated Python file. (main)

--- internal/codegen/models.go
+++ internal/codegen/models.go
@@ -1,6 +1,9 @@
 package codegen
 
-import "github.com/rayakame/sqlc-gen-better-python/internal/core"
+import (
+	"fmt"
+	"github.com/rayakame/sqlc-gen-better-python/internal/core"
+)
 
 func BuildModelFile(config *core.Config, tables []core.Table) (string, []byte, error) {
 	imports := make([]string, 0)
@@ -18,6 +21,8 @@ func BuildModelFile(config *core.Config, tables []core.Table) (string, []byte, e
 		imports = append(imports, "from dataclasses import dataclass")
 	case core.ModelTypeAttrs:
 		imports = append(imports, "import attrs")
+	default:
+		return "", nil, fmt.Errorf("unsupported model type: %s", config.ModelType)
 	}
 
 	body := NewIndentStringBuilder(config.IndentChar, config.CharsPerIndentLevel)
@@ -25,5 +30,33 @@ func BuildModelFile(config *core.Config, tables []core.Table) (string, []byte, e
 	for _, imp := range imports {
 		body.WriteLine(imp)
 	}
+	for _, table := range tables {
+		body.WriteString("\n")
+		body.WriteString("\n")
+		buildModelHeader(config, table, body)
+		for _, col := range table.Columns {
+			type_ := col.Type.Type
+			if col.Type.IsList {
+				type_ = "typing.List[" + type_ + "]"
+			}
+			if col.Type.IsNullable {
+				type_ = "typing.Optional[" + type_ + "]"
+			}
+			body.WriteIndentedString(1, col.Name+": "+type_)
+			if config.ModelType == core.ModelTypeAttrs {
+				body.WriteString(" = attrs.field()")
+			}
+			body.WriteString("\n")
+		}
+	}
 	return "models.py", []byte(body.String()), nil
+}
+
+func buildModelHeader(config *core.Config, table core.Table, body *IndentStringBuilder) {
+	if config.ModelType == core.ModelTypeDataclass {
+		body.WriteLine("@dataclass()")
+	} else if config.ModelType == core.ModelTypeAttrs {
+		body.WriteLine("@attrs.define()")
+	}
+	body.WriteLine("class " + table.Name + ":")
 }
