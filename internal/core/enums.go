cf8667f5ef1209c5b5dbbe73b5b60babf385cf8d
Rayakame <github.android206@passmail.net>
2025-03-31 14:00:13 +0000

    feat(enums): Add SQLDriverType to enums The SQLDriverType is added and used to define the SQL drivers. (main)

--- internal/core/enums.go
+++ internal/core/enums.go
@@ -2,9 +2,15 @@ package core
 
 import "fmt"
 
+type SQLDriverType string
+
+func (dr *SQLDriverType) String() string {
+	return string(*dr)
+}
+
 const (
-	SQLDriverSQLite    = "sqlite"
-	SQLDriverAioSQLite = "aiosqlite"
+	SQLDriverSQLite    SQLDriverType = "sqlite"
+	SQLDriverAioSQLite SQLDriverType = "aiosqlite"
 )
 
 const (
@@ -12,21 +18,38 @@ const (
 	ModelTypeAttrs     = "attrs"
 )
 
-var asyncDrivers = map[string]bool{
-	string(SQLDriverSQLite):    false,
-	string(SQLDriverAioSQLite): true,
+var asyncDrivers = map[SQLDriverType]bool{
+	SQLDriverSQLite:    false,
+	SQLDriverAioSQLite: true,
 }
+
+var driversEngine = map[SQLDriverType]string{
+	SQLDriverSQLite:    "sqlite",
+	SQLDriverAioSQLite: "sqlite",
+}
+
 var validModelTypes = map[string]struct{}{
 	string(ModelTypeDataclass): {},
 	string(ModelTypeAttrs):     {},
 }
 
-func isDriverAsync(sqlDriver string) (bool, error) {
+func isDriverAsync(sqlDriver SQLDriverType) (bool, error) {
 	val, found := asyncDrivers[sqlDriver]
 	if !found {
 		return false, fmt.Errorf("unknown SQL driver: %s", sqlDriver)
 	}
 	return val, nil
+}
+
+func isDriverValid(sqlDriver SQLDriverType, engine string) error {
+	val, found := driversEngine[sqlDriver]
+	if !found {
+		return fmt.Errorf("unknown SQL driver: %s", sqlDriver)
+	}
+	if val != engine {
+		return fmt.Errorf("SQL driver %s does not support %s", sqlDriver, engine)
+	}
+	return nil
 }
 
 func isModelTypeValid(modelType string) error {
