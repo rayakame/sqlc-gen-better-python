10aeebd9c6ed3582fac9249f343e06bb2989ae19
Rayakame <github.android206@passmail.net>
2025-04-01 01:35:45 +0200

    refactor(sql): improve formatting in schema and queries files (main)

--- test/queries.sql
+++ test/queries.sql
@@ -1,39 +1,35 @@
 -- name: GetAuthor :one
-SELECT id, name FROM authors
+SELECT id, name
+FROM authors
 WHERE id = ? LIMIT 1;
 
 -- name: ListAuthors :many
-SELECT * FROM authors
+SELECT *
+FROM authors
 WHERE id IN (sqlc.slice('ids'))
 ORDER BY name;
 
 -- name: CreateAuthor :one
-INSERT INTO authors (
-    name, bio
-) VALUES (
-             ?, ?
-         )
-    RETURNING *;
+INSERT INTO authors (name, bio)
+VALUES (?, ?) RETURNING *;
 
 -- name: UpsertAuthorName :one
 UPDATE authors
-SET name = CASE WHEN sqlc.arg(set_name) THEN ? ELSE name END
-    RETURNING *;
+SET name = CASE WHEN sqlc.arg(set_name) THEN ? ELSE name END RETURNING *;
 
 -- name: UpdateAuthor :exec
 UPDATE authors
 set name = ?,
-    bio = ?
+    bio  = ?
 WHERE id = ?;
 
 -- name: DeleteAuthor :exec
-DELETE FROM authors
+DELETE
+FROM authors
 WHERE id = ?;
 
 -- name: UpdateAuthorT :one
 UPDATE authors
-SET
-    name = coalesce(sqlc.narg('name'), name),
-    bio = coalesce(sqlc.narg('bio'), bio)
-WHERE id = sqlc.arg('id')
-    RETURNING *;
+SET name = coalesce(sqlc.narg('name'), name),
+    bio  = coalesce(sqlc.narg('bio'), bio)
+WHERE id = sqlc.arg('id') RETURNING *;
--- test/schema.sql
+++ test/schema.sql
@@ -1,5 +1,13 @@
-CREATE TABLE authors (
-                         id   INTEGER PRIMARY KEY,
-                         name text  NOT NULL ,
-                         bio  text
+CREATE TABLE authors
+(
+    id   INTEGER PRIMARY KEY,
+    name text NOT NULL,
+    bio  text
 );
+
+CREATE TABLE tests
+(
+    id   INTEGER PRIMARY KEY,
+    name text NOT NULL,
+    bio  text
+)
