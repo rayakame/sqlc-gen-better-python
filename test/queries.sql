cc0434cce7e2ffa8e40383c7a500ace819ed6e59
Rayakame <github.android206@passmail.net>
2025-04-01 03:28:24 +0200

    feat(sqlc-gen-better-python): add GetStudentAndScores query (sqlc-embed)

--- test/queries.sql
+++ test/queries.sql
@@ -40,3 +40,8 @@ SELECT sqlc.embed(students), sqlc.embed(test_scores)
 FROM students
          JOIN test_scores ON test_scores.student_id = students.id
 WHERE students.id = ?;
+
+-- name: GetStudentAndScores :many
+SELECT sqlc.embed(students), sqlc.embed(test_scores)
+FROM students
+         JOIN test_scores ON test_scores.student_id = students.id;
