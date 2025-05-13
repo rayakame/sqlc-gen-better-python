fc9f21a09c2f190cc321e79e4b4fd109435b235b
Rayakame <github.android206@passmail.net>
2025-05-13 20:58:07 +0200

    refactor(noxfile): Add future import for annotations in noxfile.py (main)

--- noxfile.py
+++ noxfile.py
@@ -1,23 +1,20 @@
-import os
+from __future__ import annotations
+
 import pathlib
 import typing as t
 
 import nox
 from nox import options
 
-PATH_TO_PROJECT = pathlib.Path(__name__).parent / "test"
-SCRIPT_PATHS = ["noxfile.py"]
+PATH_TO_PROJECT = pathlib.Path(__name__).parent
+SCRIPT_PATHS = ["noxfile.py", PATH_TO_PROJECT / "scripts"]
 
-DRIVER_PATHS = {
-    "asyncpg": PATH_TO_PROJECT / "driver_asyncpg"
-}
+DRIVER_PATHS = {"asyncpg": PATH_TO_PROJECT / "test" / "driver_asyncpg"}
 
-SQLC_CONFIGS = [
-    "sqlc.yaml"
-]
+SQLC_CONFIGS = ["sqlc.yaml"]
 
 options.default_venv_backend = "uv"
-options.sessions = ["pyright"]
+options.sessions = ["pyright", "ruff"]
 
 
 # uv_sync taken from: https://github.com/hikari-py/hikari/blob/master/pipelines/nox.py#L48
@@ -35,10 +32,16 @@ options.sessions = ["pyright"]
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 def uv_sync(
-        session: nox.Session, /, *, include_self: bool = False, extras: t.Sequence[str] = (), groups: t.Sequence[str] = ()
+    session: nox.Session,
+    /,
+    *,
+    include_self: bool = False,
+    extras: t.Sequence[str] = (),
+    groups: t.Sequence[str] = (),
 ) -> None:
     if extras and not include_self:
-        raise RuntimeError("When specifying extras, set `include_self=True`.")
+        msg = "When specifying extras, set `include_self=True`."
+        raise RuntimeError(msg)
 
     args: list[str] = []
     for extra in extras:
@@ -49,27 +52,20 @@ def uv_sync(
         args.extend((group_flag, group))
 
     session.run_install(
-        "uv", "sync", "--frozen", *args, silent=True, env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location}
+        "uv",
+        "sync",
+        "--frozen",
+        *args,
+        silent=True,
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
     )
 
-
-@nox.session()
-def format_fix(session: nox.Session) -> None:
-    uv_sync(session, groups=["dev"])
-    session.run("python", "-m", "ruff", "format", *SCRIPT_PATHS)
-    session.run("python", "-m", "ruff", "check", *SCRIPT_PATHS, "--fix")
-
-
-@nox.session()
-def format(session: nox.Session) -> None:
-    uv_sync(session, groups=["dev"])
-    session.run("python", "-m", "ruff", "format", *SCRIPT_PATHS, "--check")
-    session.run("python", "-m", "ruff", "check", *SCRIPT_PATHS)
 
 def sqlc_generate(session: nox.Session, driver: str) -> None:
     with session.chdir(DRIVER_PATHS[driver]):
         for config in SQLC_CONFIGS:
             session.run("sqlc", "generate", "-f", config, external=True)
+
 
 @nox.session()
 def asyncpg(session: nox.Session) -> None:
@@ -84,3 +80,11 @@ def pyright(session: nox.Session) -> None:
     uv_sync(session, include_self=True, groups=["pyright"])
 
     session.run("pyright", *SCRIPT_PATHS)
+
+
+@nox.session()
+def ruff(session: nox.Session) -> None:
+    uv_sync(session, include_self=True, groups=["ruff"])
+
+    session.run("ruff", "format", *SCRIPT_PATHS)
+    session.run("ruff", "check")
