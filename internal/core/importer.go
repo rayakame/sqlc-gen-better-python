317cf520d7ded3ac472bb843e3b41edd4a8af419
Rayakame <github.android206@passmail.net>
2025-05-13 20:59:00 +0200

    feat(importer): Refactor importSpec struct in internal/core/importer.go
    - Refactor importSpec struct to include TypeChecking field for better
      organization and readability of code. Split typeChecking imports into
      separate map for clarity and maintainability.
    
    Issue: main (not available)

--- internal/core/importer.go
+++ internal/core/importer.go
@@ -2,14 +2,16 @@ package core
 
 import (
 	"fmt"
+	"github.com/rayakame/sqlc-gen-better-python/internal/typeConversion"
 	"sort"
 	"strings"
 )
 
 type importSpec struct {
-	Module string
-	Name   string
-	Alias  string
+	Module       string
+	Name         string
+	Alias        string
+	TypeChecking bool
 }
 
 func (i importSpec) String() string {
@@ -32,7 +34,7 @@ type Importer struct {
 	C       *Config
 }
 
-func (i *Importer) Imports(fileName string) []string {
+func (i *Importer) Imports(fileName string) ([]string, []string, []string) {
 	if fileName == "models.sql" {
 		return i.modelImports()
 	}
@@ -41,14 +43,12 @@ func (i *Importer) Imports(fileName string) []string {
 
 func tableUses(name string, s Table) bool {
 	for _, col := range s.Columns {
-		if name == "typing" && col.Type.IsList || name == "typing" && col.Type.IsNullable {
-			return true
-		}
 		if col.Type.Type == name {
 			return true
 		}
 	}
 	return false
+
 }
 
 func (i *Importer) getModelImportSpec() (string, importSpec, error) {
@@ -64,17 +64,34 @@ func (i *Importer) getModelImportSpec() (string, importSpec, error) {
 	}
 }
 
-func (i *Importer) modelImportSpecs() (map[string]importSpec, map[string]importSpec) {
-	modelUses := func(name string) bool {
+func (i *Importer) splitTypeChecking(pks map[string]importSpec) (map[string]importSpec, map[string]importSpec) {
+	normalImports := make(map[string]importSpec)
+	typeChecking := make(map[string]importSpec)
+	for name, val := range pks {
+		if val.TypeChecking {
+			typeChecking[name] = val
+		} else {
+			normalImports[name] = val
+		}
+	}
+	return normalImports, typeChecking
+}
+
+func (i *Importer) modelImportSpecs() (map[string]importSpec, map[string]importSpec, map[string]importSpec) {
+	modelUses := func(name string) (bool, bool) {
 		for _, table := range i.Tables {
 			if tableUses(name, table) {
-				return true
+				return true, true
 			}
 		}
-		return false
+		return false, false
 	}
 
 	std := stdImports(modelUses)
+	std, typeChecking := i.splitTypeChecking(std)
+	if len(typeChecking) != 0 {
+		std["typing"] = importSpec{Module: "typing"}
+	}
 	modelName, modelImport, err := i.getModelImportSpec()
 	if err == nil {
 		std[modelName] = modelImport
@@ -85,53 +102,56 @@ func (i *Importer) modelImportSpecs() (map[string]importSpec, map[string]importS
 
 	pkg := make(map[string]importSpec)
 
-	return std, pkg
+	return std, typeChecking, pkg
 }
 
-func queryValueUses(name string, qv QueryValue) bool {
+func (i *Importer) queryValueUses(name string, qv QueryValue) (bool, bool) {
 	if !qv.IsEmpty() {
-		if name == "typing" && qv.Typ.IsList {
-			return true
-		}
-		if name == "typing" && qv.Typ.IsNullable {
-			return true
-		}
 		if qv.IsStruct() && qv.EmitStruct() {
 			if tableUses(name, *qv.Table) {
-				return true
+				return true, false
 			}
 		} else {
 			if qv.Typ.Type == name {
-				return true
+				if i.C.SqlDriver == SQLDriverAsyncpg {
+					if _, found := typeConversion.AsyncpgDoTypeConversion()[qv.Typ.SqlType]; found {
+						return true, false
+					} else {
+						return true, true
+					}
+				}
+				return true, false
 			}
 		}
 	}
-	return false
+	return false, false
 }
 
-func (i *Importer) queryImportSpecs(fileName string) (map[string]importSpec, map[string]importSpec, map[string]importSpec) {
-	queryUses := func(name string) bool {
+func (i *Importer) queryImportSpecs(fileName string) (map[string]importSpec, map[string]importSpec, map[string]importSpec, map[string]importSpec) {
+	queryUses := func(name string) (bool, bool) {
 		for _, q := range i.Queries {
 			//if q.SourceName != fileName { TODO q.SourceName is the name of the sql file
 			//	continue
 			//}
-			if queryValueUses(name, q.Ret) {
-				return true
+			if val1, val2 := i.queryValueUses(name, q.Ret); val1 {
+				return val1, val2
 			}
 			for _, arg := range q.Args {
-				if queryValueUses(name, arg) {
-					return true
+				if val1, val2 := i.queryValueUses(name, arg); val1 {
+					return val1, val2
 				}
 			}
 		}
-		return false
+		return false, false
 	}
 
 	std := stdImports(queryUses)
+	std, typeChecking := i.splitTypeChecking(std)
+	typeChecking[i.C.SqlDriver.String()] = importSpec{Module: i.C.SqlDriver.String()}
+	std["typing"] = importSpec{Module: "typing"}
 
 	pkg := make(map[string]importSpec)
 	loc := make(map[string]importSpec)
-	pkg[i.C.SqlDriver.String()] = importSpec{Module: i.C.SqlDriver.String()}
 
 	queryValueModelImports := func(qv QueryValue) {
 		if qv.IsStruct() && qv.EmitStruct() {
@@ -151,47 +171,51 @@ func (i *Importer) queryImportSpecs(fileName string) (map[string]importSpec, map
 
 	loc["models"] = importSpec{Module: i.C.Package, Name: "models"}
 
-	return std, pkg, loc
+	return std, typeChecking, pkg, loc
 }
 
-func (i *Importer) queryImports(fileName string) []string {
-	std, pkg, loc := i.queryImportSpecs(fileName)
+func (i *Importer) queryImports(fileName string) ([]string, []string, []string) {
+	std, typeCheck, pkg, loc := i.queryImportSpecs(fileName)
 
 	importLines := make([]string, 0)
+	typeLines := make([]string, 0)
+	packageLines := make([]string, 0)
 	if len(std) != 0 {
-		importLines = append(importLines, buildImportBlock(std))
+		importLines = append(importLines, buildImportBlock(std)...)
+	}
+	if len(typeCheck) != 0 {
+		typeLines = append(typeLines, buildImportBlock(typeCheck)...)
 	}
 	if len(pkg) != 0 {
-		if len(importLines) != 0 {
-			importLines = append(importLines, "")
-		}
-		importLines = append(importLines, buildImportBlock(pkg))
+		packageLines = append(packageLines, buildImportBlock(pkg)...)
 	}
 	if len(loc) != 0 {
-		if len(importLines) != 0 {
-			importLines = append(importLines, "")
+		if len(packageLines) != 0 {
+			packageLines = append(packageLines, "")
 		}
-		importLines = append(importLines, buildImportBlock(loc))
+		packageLines = append(packageLines, buildImportBlock(loc)...)
 	}
-	return importLines
+	return importLines, typeLines, packageLines
 }
 
-func (i *Importer) modelImports() []string {
-	std, pkg := i.modelImportSpecs()
+func (i *Importer) modelImports() ([]string, []string, []string) {
+	std, typeCheck, pkg := i.modelImportSpecs()
 	importLines := make([]string, 0)
+	typeLines := make([]string, 0)
+	packageLines := make([]string, 0)
 	if len(std) != 0 {
-		importLines = append(importLines, buildImportBlock(std))
+		importLines = append(importLines, buildImportBlock(std)...)
+	}
+	if len(typeCheck) != 0 {
+		typeLines = append(typeLines, buildImportBlock(typeCheck)...)
 	}
 	if len(pkg) != 0 {
-		if len(importLines) != 0 {
-			importLines = append(importLines, "")
-		}
-		importLines = append(importLines, buildImportBlock(pkg))
+		packageLines = append(packageLines, buildImportBlock(pkg)...)
 	}
-	return importLines
+	return importLines, typeLines, packageLines
 }
 
-func buildImportBlock(pkgs map[string]importSpec) string {
+func buildImportBlock(pkgs map[string]importSpec) []string {
 	pkgImports := make([]importSpec, 0)
 	fromImports := make(map[string][]string)
 	for _, is := range pkgs {
@@ -217,20 +241,29 @@ func buildImportBlock(pkgs map[string]importSpec) string {
 		importStrings = append(importStrings, fmt.Sprintf("from %s import %s", modName, nameString))
 	}
 	sort.Strings(importStrings)
-	return strings.Join(importStrings, "\n")
+	return importStrings
 }
 
-func stdImports(uses func(name string) bool) map[string]importSpec {
+func stdImports(uses func(name string) (bool, bool)) map[string]importSpec {
 	std := make(map[string]importSpec)
-	std["typing"] = importSpec{Module: "typing"}
-	if uses("decimal.Decimal") {
-		std["decimal"] = importSpec{Module: "decimal"}
+	std["collections"] = importSpec{Module: "collections.abc", TypeChecking: true}
+	if use, typeChecking := uses("decimal.Decimal"); use {
+		std["decimal"] = importSpec{Module: "decimal", TypeChecking: typeChecking}
 	}
-	if uses("datetime.date") || uses("datetime.time") || uses("datetime.datetime") || uses("datetime.timedelta") {
-		std["datetime"] = importSpec{Module: "datetime"}
+	if use, typeChecking := uses("datetime.date"); use {
+		std["datetime"] = importSpec{Module: "datetime", TypeChecking: typeChecking}
 	}
-	if uses("uuid.UUID") {
-		std["uuid"] = importSpec{Module: "uuid"}
+	if use, typeChecking := uses("datetime.time"); use {
+		std["datetime"] = importSpec{Module: "datetime", TypeChecking: typeChecking}
+	}
+	if use, typeChecking := uses("datetime.datetime"); use {
+		std["datetime"] = importSpec{Module: "datetime", TypeChecking: typeChecking}
+	}
+	if use, typeChecking := uses("datetime.timedelta"); use {
+		std["datetime"] = importSpec{Module: "datetime", TypeChecking: typeChecking}
+	}
+	if use, typeChecking := uses("uuid.UUID"); use {
+		std["uuid"] = importSpec{Module: "uuid", TypeChecking: typeChecking}
 	}
 	return std
 }
