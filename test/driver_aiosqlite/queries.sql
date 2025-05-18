34701df0eae83e9ce978076db07662ddc2979913
Rayakame <github.android206@passmail.net>
2025-05-19 01:26:50 +0200

    fix(sqlc-gen-better-python): update sha256 checksum in sqlc.yaml files

--- test/driver_aiosqlite/queries.sql
+++ test/driver_aiosqlite/queries.sql
@@ -1,2 +1,2 @@
 -- name: GetOneTestSqliteTypes :one
-SELECT id FROM test_sqlite_types WHERE date_test = ?;
+SELECT id FROM test_sqlite_types WHERE date_test = ? AND bool_test = ? AND datetime_test = ?;
